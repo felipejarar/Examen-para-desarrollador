ca1e2983a872b800733f26daed602130663b8783
felipejarar <felipe.jara.r@usach.cl>
2020-09-29 06:53:43 -0300

    Added Activity Card Component

---
+++ angular-app/src/app/activity-card/activity-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-card',
+  templateUrl: './activity-card.component.html',
+  styleUrls: ['./activity-card.component.css']
+})
+export class ActivityCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivityCardComponent } from './activity-card/activity-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivityCardComponent,
   ],
   imports: [
     BrowserModule,
