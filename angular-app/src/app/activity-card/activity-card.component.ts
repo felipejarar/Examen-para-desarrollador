587ea889b4fc55b198132ee55103cf6cb938fee8
felipejarar <felipe.jara.r@usach.cl>
2020-09-29 08:03:10 -0300

    Added Material Angular package

--- angular-app/src/app/activity-card/activity-card.component.ts
+++ angular-app/src/app/activity-card/activity-card.component.ts
@@ -9,7 +9,20 @@ export class ActivityCardComponent implements OnInit {
 
   constructor() { }
 
+  displayOptionsButton : boolean;
+
   ngOnInit(): void {
+    this.displayOptionsButton = false;
+  }
+
+  OnMouseEnterBehaviour(): void {
+    this.displayOptionsButton = true;
+    console.log(this.displayOptionsButton);
+  }
+
+  OnMouseLeaveBehaviour(): void{
+    this.displayOptionsButton = false;
+    console.log(this.displayOptionsButton);
   }
 
 }
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 import { ActivityCardComponent } from './activity-card/activity-card.component';
 
 
@@ -13,7 +16,8 @@ import { ActivityCardComponent } from './activity-card/activity-card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
