f92e4a8639143663b0e7541e2f90f7b03c16d578
felipejarar <felipe.jara.r@usach.cl>
2020-09-29 16:17:17 -0300

    Updated Activity Card Component. Added Activity Class

--- angular-app/src/app/activity-card/activity-card.component.ts
+++ angular-app/src/app/activity-card/activity-card.component.ts
@@ -1,26 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
-
+import { Activity } from './activity';
 @Component({
   selector: 'app-activity-card',
   templateUrl: './activity-card.component.html',
   styleUrls: ['./activity-card.component.css']
 })
+
 export class ActivityCardComponent implements OnInit {
 
   constructor() { }
 
   faPencilAlt = faPencilAlt;
 
-  card : any = {
-    title : "Lorem Ipsum",
-    description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu magna ut odio interdum sollicitudin. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed euismod interdum ex in tempus. Praesent eget nisi a neque fringilla rutrum. Maecenas orci lorem, euismod non placerat id, sollicitudin at nibh. Donec eu mauris condimentum, tincidunt nisl non, efficitur nisi. Maecenas ultricies ipsum in turpis finibus dapibus. Nullam porta eros sed urna dictum, et semper ex sagittis"
-  }
-
+  @Input() card : Activity;
 
   ngOnInit(): void {
-  
-  }
+    if (!this.card){
+      this.card = new Activity();
+      this.card.title = "title";
+    }
+   }
 
 
 }
--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Activity } from './activity-card/activity';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-app';
+
+  test_activity = new Activity();
+  
+
 }
