069bf9efa2498febc8428dfa231596fa64655e5e
felipejarar <felipe.jara.r@usach.cl>
2020-09-29 09:28:29 -0300

    Removed Angular Material; Added Activity Card Component; Added Fontawesome package

--- angular-app/src/app/activity-card/activity-card.component.ts
+++ angular-app/src/app/activity-card/activity-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-activity-card',
@@ -9,20 +10,17 @@ export class ActivityCardComponent implements OnInit {
 
   constructor() { }
 
-  displayOptionsButton : boolean;
+  faPencilAlt = faPencilAlt;
+
+  card : any = {
+    title : "Lorem Ipsum",
+    description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu magna ut odio interdum sollicitudin. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed euismod interdum ex in tempus. Praesent eget nisi a neque fringilla rutrum. Maecenas orci lorem, euismod non placerat id, sollicitudin at nibh. Donec eu mauris condimentum, tincidunt nisl non, efficitur nisi. Maecenas ultricies ipsum in turpis finibus dapibus. Nullam porta eros sed urna dictum, et semper ex sagittis"
+  }
+
 
   ngOnInit(): void {
-    this.displayOptionsButton = false;
+  
   }
 
-  OnMouseEnterBehaviour(): void {
-    this.displayOptionsButton = true;
-    console.log(this.displayOptionsButton);
-  }
-
-  OnMouseLeaveBehaviour(): void{
-    this.displayOptionsButton = false;
-    console.log(this.displayOptionsButton);
-  }
 
 }
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +17,7 @@ import { ActivityCardComponent } from './activity-card/activity-card.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatIconModule
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
