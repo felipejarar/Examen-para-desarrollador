e00c61636b1e2391684a0fd054dda61c092dfda8
felipejarar <felipe.jara.r@usach.cl>
2020-09-29 21:00:52 -0300

    Create Mock data and Fake HTTP Request handler

--- angular-app/src/app/core/mocks/mock-error.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MockError } from './mock-error';
-
-describe('MockError', () => {
-  it('should create an instance', () => {
-    expect(new MockError()).toBeTruthy();
-  });
-});
---
+++ angular-app/src/app/core/mocks/mock-users.ts
@@ -0,0 +1,20 @@
+import { User } from '../models/user.model';
+
+export const USERS : User[] = [
+    {
+        id : 1,
+        username : "johndoe",
+        email : "john@doe.com",
+        password : "password"
+    },{
+        id : 2,
+        username : "karendoe",
+        email : "karen@doe.com",
+        password : "password"
+    },{
+        id : 3,
+        username : "stevedoe",
+        email : "steve@doe.com",
+        password : "password"     
+    }
+]
