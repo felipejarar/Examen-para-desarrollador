557b6c0c3edf97f8d663d0974ed3a801203ce240
felipejarar <felipe.jara.r@usach.cl>
2020-10-01 09:58:18 -0300

    Adding Spring Backend

---
+++ spring-app-backend/src/main/java/com/springapp/backend/apirest/SpringAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppBackendApplication.class, args);
+	}
+
+}
